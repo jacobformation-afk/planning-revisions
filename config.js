19cc0d8c76d9f71f2108fbfac455084f9c37febf
jacobformation-afk <jacob.formation@gmail.com>
2025-09-02 10:28:19 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js
+// REMPLACE par tes valeurs (Settings → API)
+window.SUPABASE_URL = "https://smxqtzbqachmzefyjpuj.supabase.co";   // <- Project URL
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNteHF0emJxYWNobXplZnlqcHVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY3OTY5ODMsImV4cCI6MjA3MjM3Mjk4M30.F2X2j_YqSlvMwr8Pa9PzFt3p0c4LlrBCE95FkyFUwVc.";             // <- anon public key
